579e6e9d9394c92297d4a642020c17203dea421f
harylax <haryandr23@gmail.com>
2026-06-02 10:02:27 +0300

    update

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,10 +1,11 @@
-from config_parser import MazeConfig
-from mazegen import MazeGen, MazeGenDFS, MazeGenPrim
+from config_parser import MazeConfig, ConfigError
+from mazegen import MazeGen, MazeGenDFS, MazeGenPrim, MazeGenError
 from output_writer import write_maze
-from mlx_renderer import MazeMLX
+from mlx_renderer import MazeMLX, MazeRendererError
+from sys import stderr
 
 
-if __name__ == "__main__":
+def main() -> None:
     config = MazeConfig()
     config.parse_config()
     if config.algo.lower() == 'prim':
@@ -16,3 +17,14 @@ if __name__ == "__main__":
     visual = MazeMLX(maze)
     visual.render()
     visual.run()
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except ConfigError as err:
+        print(f"Configuration Error: {err}", file=stderr)
+    except MazeGenError as err:
+        print(f"Maze Generation Error: {err}", file=stderr)
+    except MazeRendererError as err:
+        print(f"Minilibx Error: {err}", file=stderr)
